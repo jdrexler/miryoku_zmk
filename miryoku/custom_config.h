99dca297b0f59ec927c914985ed54b7501825285
Johannes Drexler <mail@jdrexler.net>
2023-07-15 15:30:53 +0200

    Fix umlaut

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,13 +1,13 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-/* German umlauts */
+/* German umlauts 
 ZMK_UNICODE_PAIR(   de_ae,      N0, N0,  E, N4,    N0, N0,  C, N4)
 ZMK_UNICODE_PAIR(   de_oe,      N0, N0,  F, N6,    N0, N0,  D, N6)
 ZMK_UNICODE_PAIR(   de_ue,      N0, N0,  F,  C,    N0, N0,  D,  C)
 ZMK_UNICODE_SINGLE( de_eszett,  N0, N0,  D,  F)
 
-{
+\ {
     combos {
         compatible = "zmk,combos";
         combo_esc {
@@ -17,3 +17,4 @@ ZMK_UNICODE_SINGLE( de_eszett,  N0, N0,  D,  F)
         };
     };
 };
+*/
