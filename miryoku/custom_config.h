93538ef08be48f4b61a6d16cd470e798ada14cdb
Johannes Drexler <mail@jdrexler.net>
2023-07-16 09:37:48 +0200

    Fix umlaut

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,7 +7,7 @@ ZMK_UNICODE_PAIR(   de_oe,      N0, N0,  F, N6,    N0, N0,  D, N6)
 ZMK_UNICODE_PAIR(   de_ue,      N0, N0,  F,  C,    N0, N0,  D,  C)
 ZMK_UNICODE_SINGLE( de_eszett,  N0, N0,  D,  F)
 
-#define MIRYOKU_LAYER_BASE\
+#define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp G,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SEMI,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp D,             &kp H,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp K,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
